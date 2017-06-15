4adcad487a446bcba64c41629c4f47da31a8c95b
User <Hasacul@users.noreply.github.com>
2017-06-15 22:22:37 +0200

    Test init

--- flashcards/MainWindow.xaml.cs
+++ flashcards/MainWindow.xaml.cs
@@ -13,10 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace flashcards
+namespace Flashcards
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
--- flashcards/Properties/Settings.Designer.cs
+++ flashcards/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace flashcards.Properties
+namespace Flashcards.Properties
 {
 
 
