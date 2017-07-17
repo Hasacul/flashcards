9eb5a573cc7b05f43cb353d85f17049cec1e92de
Hasacul <hasacul@users.noreply.github.com>
2017-07-17 13:38:14 +0200

    added edit selected list view

---
+++ Flashcards/files/fileManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Flashcards.files
+{
+    class fileManager
+    {
+        protected string fileName;
+        protected string[] fileContent;                                         
+        List<string> fileContents = new List<string>();                         //list of pairs
+        public string folderLocation = "profiles/";
+
+        public void saveFile(string fileName,string profile)
+        {
+            if (profile!=null &&(profile.Length > 0 && fileName.Length>0))
+            {
+                if (!doesFileExist(fileName, profile))
+                {
+                    string filePath = this.folderLocation + profile + "/" + fileName + ".txt";
+                    FileInfo file = new FileInfo(filePath);
+                    file.Directory.Create();
+                    File.WriteAllLines(@filePath, this.fileContents.ToArray());
+                }
+                else
+                {
+                    MessageBox.Show("File already exists. Use another name.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid profile name or file name.");
+            }
+        }
+
+        public void addToContent(string stringToAdd)
+        {
+           fileContents.Add(stringToAdd);
+        }
+
+        public string getContents()
+        {
+            return string.Join("\n",fileContents.ToArray());
+        }
+
+        public void createNewProfile(string profileName)
+        {
+            if (!this.doesProfileExists(profileName, this.getProfiles()))
+            {
+                DirectoryInfo DirInfo = new DirectoryInfo(this.folderLocation);
+                DirInfo.CreateSubdirectory(profileName);
+            }
+        }
+
+        public List<String> getProfiles()
+        {
+            Directory.CreateDirectory(folderLocation);                                                          //creates profiles if not exists
+            string[] directories = Directory.GetDirectories(folderLocation);
+            return directories.ToList();
+        }
+
+        public List<String> getFiles(string profile)
+        {
+            List<string> fil = new List<string>();
+            string[] files = Directory.GetFiles(this.folderLocation + profile);
+            for(int i = 0; i < files.Length; i++)
+            {
+                fil.Add(Path.GetFileName(files[i]));
+            }
+            return fil;
+        }
+
+        public bool doesProfileExists(string profileName,List<string> profiles)
+        {
+            return profiles.Contains(this.folderLocation + profileName);
+        }
+        public bool doesFileExist(string fileName, string profile)
+        {
+            List<string> fileList = getFiles(profile);
+            return fileList.Contains(fileName+".txt");
+        }
+
+        public List<string> readFromFile(string fileName, string profile)
+        {
+            string filePath = this.folderLocation + profile+"/"+fileName;
+            FileInfo file = new FileInfo(filePath);
+            this.fileContents.Clear();
+            var lines = File.ReadAllLines(filePath);
+            foreach(var line in lines)
+            {
+                this.fileContents.Add(line);
+            }
+            return this.fileContents;
+        }
+
+        public void deleteFile(string profile, string fileName)
+        {
+            if (profile != null && !fileName.Contains("*")&&(profile.Length > 0 && fileName.Length > 0))
+            {
+                if(File.Exists(this.folderLocation + profile + "/" + fileName))
+                {
+                    MessageBox.Show("Deleting file " + fileName + " from profile " + profile);
+                    File.Delete(this.folderLocation + profile + "/" + fileName);
+                } 
+            }
+            else
+            {
+                MessageBox.Show("Invalid profile name or file name.");
+            }
+        }
+    }
+}
---
+++ flashcards/views/editSelectedListView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Flashcards.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flashcards.views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy editSelectedListView.xaml
+    /// </summary>
+    public partial class editSelectedListView : UserControl
+    {
+        public editSelectedListView()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).DataContext = new editListViewModel();
+                }
+            }
+        }
+
+        private void saveChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    (window as MainWindow).DataContext = new editListViewModel();
+                }
+            }
+        }
+    }
+}
